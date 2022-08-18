5cc9accbe6e95f855cab326ed0c47203ef1a0fc8
Down Ramp Apps <98784052+DownRamp@users.noreply.github.com>
2022-08-18 12:22:33 -0400

    draggable and droppable

--- popup.js
+++ popup.js
@@ -1,31 +1,56 @@
 $(function () {
     ///set the date of today
     setDate();
-    // console.log("HIHIHI");
-    // const items = document.querySelectorAll('.list-group-item')
-    // const columns = document.querySelectorAll('.col-sm')
 
-    // items.forEach(item => {
-    //     item.addEventListener('dragstart', dragStart);
-    //     item.addEventListener('dragend', dragEnd);
-    // });
+    let dragged;
 
-    // function dragStart() {
-    //     console.log('drag started');
-    // }
-    // function dragEnd() {
-    //     console.log('drag ended');
-    // }
-    function onDragStart(event) {
-        event
-          .dataTransfer
-          .setData('text/plain', event.target.id);
-
-        event
-          .currentTarget
-          .style
-          .backgroundColor = 'yellow';
+    /* events fired on the draggable target */
+    document.addEventListener("drag", (event) => {
+      console.log("dragging");
+    });
+    
+    document.addEventListener("dragstart", (event) => {
+      // store a ref. on the dragged elem
+      dragged = event.target;
+      // make it half transparent
+      event.target.classList.add("dragging");
+    });
+    
+    document.addEventListener("dragend", (event) => {
+      // reset the transparency
+      event.target.classList.remove("dragging");
+    });
+    
+    /* events fired on the drop targets */
+    document.addEventListener("dragover", (event) => {
+      // prevent default to allow drop
+      event.preventDefault();
+    }, false);
+    
+    document.addEventListener("dragenter", (event) => {
+      // highlight potential drop target when the draggable element enters it
+      if (event.target.classList.contains("dropzone")) {
+        event.target.classList.add("dragover");
       }
+    });
+    
+    document.addEventListener("dragleave", (event) => {
+      // reset background of potential drop target when the draggable element leaves it
+      if (event.target.classList.contains("dropzone")) {
+        event.target.classList.remove("dragover");
+      }
+    });
+    
+    document.addEventListener("drop", (event) => {
+      // prevent default action (open as link for some elements)
+      event.preventDefault();
+      // move dragged element to the selected drop target
+      if (event.target.classList.contains("dropzone")) {
+        event.target.classList.remove("dragover");
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+      }
+    });
 
     var todoList = new Array();
     chrome.storage.sync.get(['list1'], function (val) {
